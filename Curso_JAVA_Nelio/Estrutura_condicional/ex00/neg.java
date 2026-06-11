008a5f4c0c75ea7105f258ebbc43578ae46909db
eulopes01 <desousagabriellopes5@gmail.com>
2026-06-10 21:15:47 -0300

    Novo exercio, porem esse era opcional

---
+++ Curso_JAVA_Nelio/Estrutura_condicional/ex00/neg.java
@@ -0,0 +1,25 @@
+//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não
+
+
+package Estrutura_condicional.ex00;
+public class neg
+{
+    public static void main (String[] args)
+    {
+        int num;
+
+        num = -10;
+
+        if (num < 0)
+        {
+            System.out.println("NUMERO NEGATIVO SEU MERDINHA");
+        }
+        else
+        {
+            System.out.println("NUMERO POTIVO PORRAAAAAA, VAMOOOOOO");
+        }
+
+
+    }
+
+}
---
+++ Curso_JAVA_Nelio/Estrutura_condicional/ex01/division.java
@@ -0,0 +1,20 @@
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+public class division
+{
+    public static void main(String[] args)
+    {
+        int num;
+
+        num = 4;
+
+        if (num % 2 == 0)
+        {
+            System.out.println("Numero é par");
+        }
+        else
+        {
+            System.out.println("O numero e impar");
+        }
+    }
+}
