dba462e87e367867955d79b8d611b8ff0d6c5070
eulopes01 <desousagabriellopes5@gmail.com>
2026-06-23 20:03:50 -0300

    Estrutura_repetitiva - ex terminado com sucesso

--- Curso_JAVA_Nelio/Estrutura_repetitiva/ex09/odd.java
+++ Curso_JAVA_Nelio/Estrutura_repetitiva/ex09/odd.java
@@ -1,2 +1,24 @@
 //Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
 //X, se for o caso.
+
+import java.util.Scanner;
+
+public class odd
+{
+    public static void main(String [] args)
+    {
+       int x;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o numero");
+        x = sc.nextInt();
+
+        for( int i = 1; i <= x; i++)
+        {
+             if(i % 2 == 1)
+                System.out.println(i);
+        }
+    sc.close();
+    }
+}
