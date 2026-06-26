e20b68b03ddfa48e4bfb941f357ae9aa6829ba2d
eulopes01 <desousagabriellopes5@gmail.com>
2026-06-25 21:42:15 -0300

    Estrutura repetitiva - Ex concluido

--- Curso_JAVA_Nelio/Estrutura_repetitiva/ex11/balanced.java
+++ Curso_JAVA_Nelio/Estrutura_repetitiva/ex11/balanced.java
@@ -2,3 +2,46 @@
 //de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
 //conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
 //peso 5.
+
+import java.util.Scanner;
+
+public class balanced
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        double nota;
+        double soma;
+
+        System.out.printf("Digite o numero de notas: ");
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++)
+        {
+            soma = 0;
+            for( int x = 0; x < 3; x++)
+            {
+                System.out.printf("Digite a %dº do Aluno: ", x);
+                nota = sc.nextDouble();
+
+                if(x == 0)
+                {
+                   soma = nota * 2; 
+                }
+                else if(x == 1)
+                {
+                    soma = soma + nota * 3;
+                }
+                else
+                {
+                    soma = soma + nota * 5;
+                }
+            }
+            nota = soma / 10;
+            System.out.printf("A Media do Aluno é %.2f\n", nota);
+        }
+
+    sc.close();
+    }
+}
---
+++ Curso_JAVA_Nelio/Estrutura_repetitiva/ex12/div.java
@@ -0,0 +1,47 @@
+//Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+//segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+
+
+import java.util.Scanner;
+
+public class div
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        double numb;
+        double numb1;
+        double result;
+
+        System.out.printf("Digite o numero de notas: ");
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++)
+        {
+            result = 0;
+
+            System.out.printf("Digite o primeiro número: ");
+            numb = sc.nextDouble();
+
+            System.out.printf("Digite o segundo número: ");
+            numb1 = sc.nextDouble();
+
+            if(!(numb1 == 0 ))
+            {
+                result = numb / numb1;
+                System.out.printf("Resultado: %.2f%n ", result);
+            }
+            else
+            {
+                System.out.println("divisao impossivel");
+            }
+
+            
+        }
+        sc.close();
+    }
+
+
+
+}
